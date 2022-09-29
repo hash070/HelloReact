5cbe8cd968b531524c67967b647a2608f02cddeb
hash070 <hash070@outlook.com>
2022-09-29 08:37:24 +0800

    HelloTick

--- src/index.js
+++ src/index.js
@@ -6,11 +6,26 @@ import { HelloWorld } from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <HelloWorld />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <HelloWorld />
+//   </React.StrictMode>
+// );
+
+
+// Tick
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  root.render(element);
+}
+
+setInterval(tick, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
